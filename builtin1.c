2f46742c610febf6bad01315e993eb07073e4b8f
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 00:55:27 -0700

    go

--- builtin1.c
+++ builtin1.c
@@ -3,7 +3,7 @@
 /**
  * _myhistory - displays the history list, one command by line,
  *  preceded with line numbers, starting at 0.
- * @info: Structure containing potential arguments. 
+ * @info: Structure containing potential arguments.
  * Used to maintain constant function prototype.
  * Return: Always 0
  **/
@@ -83,7 +83,7 @@ int print_alias(list_t *node)
 
 /**
  * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. 
+ * @info: Structure containing potential arguments.
  * Used to maintain constant function prototype.
  * Return: Always 0
  **/
