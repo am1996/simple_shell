06698172cce126e889d489008819de79b8f41bd8
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 00:07:30 -0700

    go

--- errors1.c
+++ errors1.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- *  _erratoi - converts a string to an integer
- *  s: the string to be converted
- *  Return: 0 if no numbers in string, converted number otherwise -1 on error
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise -1 on error
  **/
 
 int _erratoi(char *s)
