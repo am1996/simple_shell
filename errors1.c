670de5455b51ca4a5bb1c8ea90a22d796c2a6868
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 00:09:51 -0700

    go

--- errors1.c
+++ errors1.c
@@ -28,12 +28,12 @@ int _erratoi(char *s)
 	return (result);
 }
 
-/*
+/**
  *  print_error - prints an error message
- *  info: the parameter & return info struct
- *  estr: string containing specified error type
+ *  @info: the parameter & return info struct
+ *  @estr: string containing specified error type
  *  Return: 0 if no numbers in string, converted number otherwise -1 on error
-*/
+ **/
 
 void print_error(info_t *info, char *estr)
 {
@@ -46,12 +46,12 @@ void print_error(info_t *info, char *estr)
 	_eputs(estr);
 }
 
-/*
+/**
  * print_d - function prints a decimal (integer) number (base 10)
  * input: the input
  * fd: the filedescriptor to write to
  * Return: number of characters printed
-*/
+ **/
 
 int print_d(int input, int fd)
 {
@@ -85,13 +85,13 @@ int print_d(int input, int fd)
 	return (count);
 }
 
-/*
+/**
  * convert_number - converter function, a clone of itoa
  * num: number
  * base: base
  * flags: argument flags
  * Return: string
-*/
+ **/
 
 char *convert_number(long int num, int base, int flags)
 {
@@ -121,11 +121,11 @@ char *convert_number(long int num, int base, int flags)
 	return (ptr);
 }
 
-/*
+/**
  * remove_comments - function replaces first instance of '#' with '\0'
- * buf: address of the string to modify
+ * @buf: address of the string to modify
  * Return: Always 0;
-*/
+ **/
 
 void remove_comments(char *buf)
 {
