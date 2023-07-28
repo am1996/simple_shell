9433769251a3d0334c4c90e90ea5c24957dbc422
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 00:47:38 -0700

    go

--- builtin.c
+++ builtin.c
@@ -31,7 +31,7 @@ int _myexit(info_t *info)
 
 /**
  * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. 
+ * @info: Structure containing potential arguments.
  * Used to maintain constant function prototype.
  * Return: Always 0
  **/
@@ -82,7 +82,7 @@ int _mycd(info_t *info)
 
 /**
  * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. 
+ * @info: Structure containing potential arguments.
  * Used to maintain constant function prototype.
  * Return: Always 0
  **/
