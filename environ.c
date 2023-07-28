26bfa5855fd70a0224522c972d30c63970cc56c4
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 00:33:36 -0700

    go

--- environ.c
+++ environ.c
@@ -2,7 +2,8 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
  * Return: Always 0
 **/
 
@@ -36,7 +37,8 @@ char *_getenv(info_t *info, const char *name)
 
 /**
  *  _mysetenv - Initialize a new environment variable,or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments.
+ * Used to maintain constant function prototype.
  * Return: Always 0
 */
 
@@ -54,7 +56,8 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. 
+ * Used to maintain constant function prototype.
  * Return: Always 0
  **/
 
@@ -75,7 +78,8 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. 
+ * Used to maintain constant function prototype.
  * Return: Always 0
  **/
 
