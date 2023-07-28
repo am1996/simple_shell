968d2e822e1b2d63e2631201d54a34df475dc9c8
am1996 <ahmedmagdy92@yahoo.com>
2023-07-28 04:35:35 -0700

    go

--- getinfo.c
+++ getinfo.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
-/*
-  clear_info - initializes info_t struct
-  info: struct address
-*/
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ **/
 
 void clear_info(info_t *info)
 {
@@ -13,11 +13,11 @@ void clear_info(info_t *info)
 	info->argc = 0;
 }
 
-/*
-  set_info - initializes info_t struct
-  info: struct address
-  av: argument vector
-*/
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ **/
 
 void set_info(info_t *info, char **av)
 {
@@ -46,11 +46,11 @@ void set_info(info_t *info, char **av)
 	}
 }
 
-/*
-  free_info - frees info_t struct fields
-  info: struct address
-  all: true if freeing all fields
-*/
+/**
+ * free_info - frees info_t struct fields
+ * @info: struct address
+ * @all: true if freeing all fields
+ **/
 
 void free_info(info_t *info, int all)
 {
